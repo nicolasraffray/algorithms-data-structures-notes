c5b5bdf659adb251211fbaa1c7223f2ded1f28e0
nicolasraffray <nraffray@gmail.com>
2020-06-26 14:06:07 +0100

    returns int and can do simple odd array

--- python_functions/lib/binary_search.py
+++ python_functions/lib/binary_search.py
@@ -1,3 +1,7 @@
 
-def binary_search(array):
-  return array
+def binary_search(array, item):
+  low = 0
+  high = len(array) -1
+  guess = (high + low) /2 
+  guess = int(guess)
+  return guess
--- python_functions/tests/test_binary.py
+++ python_functions/tests/test_binary.py
@@ -3,13 +3,16 @@ from ..lib.binary_search import binary_search
 
 class TestBinarySearch:
 
-  def test_takesArray(self):
+  def test_takes_array_and_returns_int(self):
     array = [1,1,2,3,5]
-    result = binary_search(array)
-    print(result)
-    assert type(result) is list
+    result = binary_search(array, 3)
+    
+    assert type(result) is int or None 
 
+  def test_simple_list(self):
+    array = [1,2,4]
+    item = 2
+    result = binary_search(array, item)
+    assert result == 1
 
-# test = BinarySearch()
-# test.takesArray()
 
