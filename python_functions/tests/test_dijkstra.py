2c570139deb8ba579c991a73074758a530d8e321
nicolasraffray <nraffray@gmail.com>
2020-07-14 17:10:04 +0100

    .

--- python_functions/tests/test_dijkstra.py
+++ python_functions/tests/test_dijkstra.py
@@ -1,16 +1,5 @@
 import pytest
-from ..lib.dijkstra import Dijkstra
+# from ..lib.dijkstra import Dijkstra
 
 
-graph = {'start': {'a': 6, 'b': 2}, 'b': {'a': 3, 'fin': 5}, 'fin': {}}
-
-
-
-def test_keeps_track_of_unvisited_nodes():
-    d = Dijkstra(graph)
-    results = d.get_unvisited_nodes({'a', 'start'})
-    assert results == ['b', 'fin']
-
-  
-      
-
+# graph = {'start': {'a': 6, 'b': 2}, 'b': {'a': 3, 'fin': 5}, 'fin': {}}
