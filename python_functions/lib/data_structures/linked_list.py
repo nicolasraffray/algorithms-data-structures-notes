e5c539495cdac390ece82d1440e2ff181db4fc48
nicolasraffray <nraffray@gmail.com>
2020-07-14 21:12:48 +0100

    list can link nodes

--- python_functions/lib/data_structures/linked_list.py
+++ python_functions/lib/data_structures/linked_list.py
@@ -3,3 +3,9 @@ class Node():
     def __init__(self, value=None):
         self.value = value
         self.next = None
+
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
--- python_functions/tests/test_linked_list.py
+++ python_functions/tests/test_linked_list.py
@@ -1,8 +1,23 @@
 import pytest
-from ..lib.data_structures.linked_list import Node
+from ..lib.data_structures.linked_list import Node, LinkedList
 
 
 def test_node():
     node = Node(1)
     assert node.value == 1
     assert node.next == None
+
+
+def test_linked_list_takes_nodes():
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+
+    linked_list = LinkedList()
+    linked_list.head = node1
+    assert linked_list.head.value == 1
+
+    node1.next = node2
+    node2.next = node3
+
+    assert linked_list.head.next.value == 2
