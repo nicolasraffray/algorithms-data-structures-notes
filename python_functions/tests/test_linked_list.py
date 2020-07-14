6fcfcbe9631de48f65525e65e1f0b686757538b8
nicolasraffray <nraffray@gmail.com>
2020-07-14 21:38:07 +0100

    light test refactor

--- python_functions/tests/test_linked_list.py
+++ python_functions/tests/test_linked_list.py
@@ -2,22 +2,30 @@ import pytest
 from ..lib.data_structures.linked_list import Node, LinkedList
 
 
-def test_node():
-    node = Node(1)
-    assert node.value == 1
-    assert node.next == None
-
-
-def test_linked_list_takes_nodes():
+@pytest.fixture(scope="module", autouse=True)
+def generate_list():
     node1 = Node(1)
     node2 = Node(2)
     node3 = Node(3)
 
     linked_list = LinkedList()
     linked_list.head = node1
-    assert linked_list.head.value == 1
-
     node1.next = node2
     node2.next = node3
 
+    return [linked_list, node1]
+
+
+def test_node(generate_list):
+    node1 = generate_list[1]
+    assert node1.value == 1
+
+
+def test_linked_list_takes_nodes(generate_list):
+    linked_list = generate_list[0]
+    assert linked_list.head.value == 1
     assert linked_list.head.next.value == 2
+
+
+def test_print_node_list_values():
+    pass
