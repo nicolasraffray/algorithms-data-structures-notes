847e5253cf2a2deb6dda264b94edbf051bf1c383
nicolasraffray <nraffray@gmail.com>
2020-07-14 17:09:49 +0100

    dijkstra func

--- python_functions/lib/dijkstra.py
+++ python_functions/lib/dijkstra.py
@@ -1,23 +1,57 @@
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
 
 
-class Dijkstra:
+def dijkstra(graph, start, end):
+    print(graph)
+    costs = {}
+    parents = {}
+    unseen_nodes = graph
+    infinity = float('inf')
+    final_path = []
 
-  def __init__(self, graph):
-    self.graph = graph 
-    self.costs = {}
-  
-  def get_unvisited_nodes(self, visited):
-    unvisited = []
-    for i in list(self.graph.keys()):
-      if i not in visited:
-        unvisited.insert(len(unvisited), i)
-    return unvisited
+    for node in unseen_nodes:
+        costs[node] = infinity
+    costs[start] = 0
 
-  def generate_costs(self):
-    for nested in self.graph.values():
-      if nested is int:
-        nested = float('inf')
-      while type(nested) is dict and bool(nested):
-        self.generate_costs()
-    
+    while unseen_nodes:
+        min_node = None
+        for node in unseen_nodes:
+            if min_node == None:
+                min_node = node
+            elif costs[node] < costs[min_node]:
+                print("Here is the cost: ", costs[node])
+                min_node = node
 
+        for child, weight in graph[min_node].items():
+            if weight + costs[min_node] < costs[child]:
+                costs[child] = weight + costs[min_node]
+                parents[child] = min_node
+        unseen_nodes.pop(min_node)
+    # print(parents)
+    # print(costs)
+    current_node = end
+    while current_node != start:
+        try:
+            final_path.insert(0, current_node)
+            current_node = parents[current_node]
+        except KeyError:
+            print('path cannot be reached')
+            break
+    final_path.insert(0, start)
+    return final_path
+
+
+path = dijkstra(graph, 'start', 'fin')
+print(path)
